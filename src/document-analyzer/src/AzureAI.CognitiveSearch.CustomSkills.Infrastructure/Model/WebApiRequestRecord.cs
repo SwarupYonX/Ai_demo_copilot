2f5a3c37775474e44f5bf6ce9f04afbb200fa0bf
SwarupYonX <swaruptechranjan@gmail.com>
2024-05-15 12:45:09 +0530

    demo modifications

--- src/document-analyzer/src/AzureAI.CognitiveSearch.CustomSkills.Infrastructure/Model/WebApiRequestRecord.cs
+++ src/document-analyzer/src/AzureAI.CognitiveSearch.CustomSkills.Infrastructure/Model/WebApiRequestRecord.cs
@@ -2,9 +2,9 @@
 
 namespace AzureAI.CognitiveSearch.CustomSkills.Infrastructure.Model
 {
-    public class WebApiRequestRecord
-    {
-        public string RecordId { get; set; }
-        public Dictionary<string, object> Data { get; set; } = new Dictionary<string, object>();
-    }
+    // public class WebApiRequestRecord
+    // {
+    //     public string RecordId { get; set; }
+    //     public Dictionary<string, object> Data { get; set; } = new Dictionary<string, object>();
+    // }
 }
